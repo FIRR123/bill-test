cad7199b856d1209319dbe243471c2e59b577b74
FIRR123 <1441635369@qq.com>
2024-06-04 11:27:19 +0800

    路径解析配置

--- src/index.js
+++ src/index.js
@@ -2,6 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
+import add from "@/test.js"
+let total = add(1,1)
+console.log(total)
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <App />
