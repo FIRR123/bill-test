190a6b074c4558646f8bbe926c15db5c0f1d8be0
FIRR123 <1441635369@qq.com>
2024-06-08 15:27:33 +0800

    month选择时间

--- src/pages/Month/index.js
+++ src/pages/Month/index.js
@@ -2,11 +2,24 @@ import { NavBar, DatePicker } from 'antd-mobile'
 import './index.scss'
 import { useState } from 'react'
 import classNames from 'classnames';
+import dayjs from 'dayjs';
 
 const Month = () => {
 
   // 控制弹窗是否出现
   const [dateVisible, setDateVisible] = useState(false);
+
+  // 选择的时间
+  const [chooseDate, setChooseDate] = useState(() => {
+    return dayjs(new Date()).format('YYYY-MM')
+  })
+
+  // 确认选择时间
+  const confirmDate = (date) => {
+    setDateVisible(false);
+
+    setChooseDate(dayjs(date).format('YYYY-MM'))
+  }
 
   return (
     <div className="monthlyBill">
@@ -18,7 +31,7 @@ const Month = () => {
           {/* 时间切换区域 */}
           <div className="date">
             <span className="text">
-              2023 | 3月账单
+              {chooseDate}月账单
             </span>
             {/* 根据弹窗判断箭头朝向 */}
             <span onClick={() => setDateVisible(true)} className={classNames('arrow', dateVisible && 'expand')}></span>
@@ -44,7 +57,7 @@ const Month = () => {
             title="记账日期"
             precision="month"
             visible={dateVisible}
-            onConfirm={() => setDateVisible(false)}
+            onConfirm={confirmDate}
             onCancel={() => setDateVisible(false)}
             max={new Date()}
           />
