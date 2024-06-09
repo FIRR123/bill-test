f80cd45ef554bb6a4d62d77c088a912d75d03b36
FIRR123 <1441635369@qq.com>
2024-06-09 23:38:24 +0800

    单日账单列表显示

--- src/pages/Month/components/DayBill/index.js
+++ src/pages/Month/components/DayBill/index.js
@@ -1,14 +1,15 @@
 import classNames from 'classnames'
 import './index.scss'
 import { useMemo } from 'react';
+import { billTypeToName } from '@/constants';
 
 const DailyBill = ({ date, dayBillList }) => {
 
   // 计算支出收入和结余
-  let currentResult = useMemo(()=> {
-    if(dayBillList) {
-      let pay = dayBillList.filter(item => item.type === "pay").reduce((a,c) => a + c.money, 0)
-      let income = dayBillList.filter(item => item.type === "income").reduce((a,c) => a + c.money, 0)
+  let currentResult = useMemo(() => {
+    if (dayBillList) {
+      let pay = dayBillList.filter(item => item.type === "pay").reduce((a, c) => a + c.money, 0)
+      let income = dayBillList.filter(item => item.type === "income").reduce((a, c) => a + c.money, 0)
       let total = pay + income
       return {
         pay, income, total
@@ -33,17 +34,32 @@ const DailyBill = ({ date, dayBillList }) => {
         <div className="oneLineOverview">
           <div className="pay">
             <span className="type">支出</span>
-            <span className="money">{currentResult.pay}</span>
+            <span className="money">{currentResult.pay.toFixed(2)}</span>
           </div>
           <div className="income">
             <span className="type">收入</span>
-            <span className="money">{currentResult.income}</span>
+            <span className="money">{currentResult.income.toFixed(2)}</span>
           </div>
           <div className="balance">
-            <span className="money">{currentResult.total}</span>
+            <span className="money">{currentResult.total.toFixed(2)}</span>
             <span className="type">结余</span>
           </div>
         </div>
+      </div>
+      {/* 单日列表 */}
+      <div className="billList">
+        {dayBillList.map(item => {
+          return (
+            <div className="bill" key={item.id}>
+              <div className="detail">
+                <div className="billType">{billTypeToName[item.useFor]}</div>
+              </div>
+              <div className={classNames('money', item.type)}>
+                {item.money.toFixed(2)}
+              </div>
+            </div>
+          )
+        })}
       </div>
     </div>
   )
